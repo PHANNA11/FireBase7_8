572019b2ab89916d3506030239d6d4ae8a527ee7
PHANNA11 <phaphanna2018@gmail.com>
2022-11-03 20:28:26 +0700

    commit realtime

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,10 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firease7_8/screens/create&update_screen.dart';
 import 'package:firease7_8/screens/login_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'dart:developer' as dev;
+
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -14,36 +17,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<String> docIDs = [];
-  Future getDataFromFireStore() async {
-    var a = await FirebaseFirestore.instance
-        .collection('Coins')
-        .get()
-        .then((value) {
-      value.docs.forEach((element) {
-        docIDs.add(element.reference.id);
-      });
-    });
-  }
-
-  Future getCoin(String docId) async {
-    final coins = FirebaseFirestore.instance.collection('Coins').doc(docId);
-
-    final snapshot = await coins.get();
-
-    if (snapshot.exists) {
-      Map<String, dynamic> data = snapshot.data() as Map<String, dynamic>;
-      dev.log(data.toString());
-    }
-  }
-
-  var fielList;
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    fielList = getDataFromFireStore();
-  }
+  CollectionReference dataCoin = FirebaseFirestore.instance.collection('Coins');
 
   @override
   Widget build(BuildContext context) {
@@ -75,44 +49,81 @@ class _HomePageState extends State<HomePage> {
         ],
       )),
       appBar: AppBar(title: const Text('HomePage')),
-      body: ListView.builder(
-        itemCount: docIDs.length,
-        itemBuilder: (context, index) {
-          CollectionReference dataCoin =
-              FirebaseFirestore.instance.collection('Coins');
-          return FutureBuilder<DocumentSnapshot>(
-            future: dataCoin.doc(docIDs[index]).get(),
-            builder: (context, snapshot) {
-              if (snapshot.hasError) {
-                return const Center(
-                  child: Icon(
-                    Icons.info,
-                    color: Colors.red,
-                    size: 50,
-                  ),
-                );
-              } else if (snapshot.connectionState == ConnectionState.waiting) {
-                return const CircularProgressIndicator();
-              } else {
-                final data = snapshot.data;
-                return data == null
-                    ? const Center(
-                        child: Text('No data...!!'),
-                      )
-                    : Card(
-                        child: ListTile(
-                          leading: CircleAvatar(
-                            backgroundImage:
-                                NetworkImage(data['logo'].toString()),
-                          ),
-                          title: Text(data['name'].toString()),
-                          trailing: Text('${data['price']}\$'),
-                        ),
-                      );
-              }
-            },
-          );
+      body: StreamBuilder(
+        stream: dataCoin.snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(
+              child: Icon(
+                Icons.info,
+                color: Colors.red,
+                size: 50,
+              ),
+            );
+          } else if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          } else {
+            final data = snapshot.data;
+            return data == null
+                ? const Center(
+                    child: Text('No data...!!'),
+                  )
+                : ListView.builder(
+                    itemCount: snapshot.data!.docs.length,
+                    itemBuilder: (context, index) {
+                      var temp = snapshot.data!.docs[index];
+                      return InkWell(
+                          onTap: () => Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => CreateAndUpdateScreen(
+                                    coinMap: temp, docId: temp.id),
+                              )),
+                          child: Slidable(
+                            key: const ValueKey(0),
+                            endActionPane: ActionPane(
+                              motion: const ScrollMotion(),
+                              //  dismissible: DismissiblePane(onDismissed: () {}),
+                              children: [
+                                SlidableAction(
+                                  onPressed: (context) async {
+                                    await FirebaseFirestore.instance
+                                        .collection('Coins')
+                                        .doc(temp.id)
+                                        .delete();
+                                  },
+                                  backgroundColor: Color(0xFFFE4A49),
+                                  foregroundColor: Colors.white,
+                                  icon: Icons.delete,
+                                  label: 'Delete',
+                                ),
+                              ],
+                            ),
+                            child: Card(
+                              child: ListTile(
+                                leading: CircleAvatar(
+                                  backgroundImage: NetworkImage(temp['logo']),
+                                ),
+                                title: Text(temp['name']),
+                                trailing: Text('${temp['price']}\$'),
+                              ),
+                            ),
+                          ));
+                    },
+                  );
+          }
         },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) =>
+                    CreateAndUpdateScreen(coinMap: null, docId: ''),
+              ));
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
