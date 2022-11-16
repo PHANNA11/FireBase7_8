f8ccc8564d4455786ed274ae6ba0734e60d51761
PHANNA11 <phaphanna2018@gmail.com>
2022-11-16 23:34:24 +0700

    gg

--- lib/screens/storage/storage_screen.dart
+++ lib/screens/storage/storage_screen.dart
@@ -6,6 +6,8 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'dart:developer' as dev;
 
+import 'package:image_picker/image_picker.dart';
+
 class StorageScreen extends StatefulWidget {
   const StorageScreen({super.key});
 
@@ -21,14 +23,41 @@ class _StorageScreenState extends State<StorageScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Storage'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                getImageFromGallary();
+              },
+              icon: const Icon(Icons.image)),
+          IconButton(
+              onPressed: () async {
+                uploadFile();
+              },
+              icon: const Icon(Icons.upload)),
+          const SizedBox(height: 40),
+        ],
       ),
-      body: Center(
-        child: retrievFile == null ? SizedBox() : Image.network(retrievFile),
+      body: Column(
+        children: [
+          // Container(
+          //   height: 300,
+          //   width: double.infinity,
+          //   color: Colors.red,
+          // ),
+          Center(
+            child:
+                retrievFile == null ? SizedBox() : Image.network(retrievFile),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
+        onPressed: () async {
+          var urlRef = firestorage.child('/File:`').child(
+              '/data/user/0/com.example.firease7_8/cache/image_picker7162668884927892.png');
+          var imageUrl = await urlRef.getDownloadURL();
+          dev.log('Download:${imageUrl}');
           setState(() {
-            getFileStorage('unnamed-14');
+            retrievFile = imageUrl;
           });
         },
         child: const Icon(Icons.download),
@@ -37,14 +66,27 @@ class _StorageScreenState extends State<StorageScreen> {
   }
 
   var retrievFile;
+  Future uploadFile() async {
+    late Reference ref;
+    final path = "$fileImage";
+    ref = FirebaseStorage.instance.ref().child(path);
+    ref.putFile(fileImage!).then((p0) async {
+      var t = await ref.child('/File:').child(fileImage!.path).getDownloadURL();
+      dev.log('Download:${t.toString()}');
+    });
+  }
 
   Future<String?> getFileStorage(String? imageName) async {
     if (imageName == null) {
+      dev.log('Empty file');
       return '';
     }
     try {
-      var urlRef = firestorage.child('$imageName.png');
+      //   dev.log('Download:$imageName');
+      var urlRef = firestorage.child('/File:`').child(
+          '/data/user/0/com.example.firease7_8/cache/image_picker7162668884927892.png');
       var imageUrl = await urlRef.getDownloadURL();
+      dev.log('Download:${imageUrl}');
       setState(() {
         retrievFile = imageUrl;
       });
@@ -55,5 +97,13 @@ class _StorageScreenState extends State<StorageScreen> {
       print(e);
       return '';
     }
+  }
+
+  // ==================================
+  getImageFromGallary() async {
+    final file = await ImagePicker().pickImage(source: ImageSource.gallery);
+    setState(() {
+      fileImage = File(file!.path);
+    });
   }
 }
